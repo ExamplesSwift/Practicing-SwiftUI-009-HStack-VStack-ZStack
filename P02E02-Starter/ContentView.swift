b937132f3bed8c209564e6d5bcfbc2d9de166569
Henry Ambicho T <yiperu@gmail.com>
2019-11-17 11:59:52 -0300

    Now change to VStack(alignment: .trailing, spacing: 20) { for see more centrade component inside VStack

--- P02E02-Starter/ContentView.swift
+++ P02E02-Starter/ContentView.swift
@@ -31,7 +31,7 @@ import SwiftUI
 struct ContentView: View {
   var body: some View {
     
-    VStack(alignment: .leading, spacing: 20) {
+    VStack(alignment: .trailing, spacing: 20) {
       Text("RegEx for Cats")
         .font(Font.system(.largeTitle, design: .rounded))
         .fontWeight(.heavy)
